41ed75082ce2ed2d5fc3deef42cffabe59c98057
Yuri Edward <yuri6037@outlook.com>
2022-06-07 14:21:05 +0200

    Refactor: moved ContentView to it's own subdirectory + divided ContentView in subviews

---
+++ BPXViewer/View/Content/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  BPXViewer
+//
+//  Created by Yuri Edwrad on 5/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Binding var document: BPXDocument
+    @State var selected = -1;
+    @StateObject var sectionState = SectionState();
+    @EnvironmentObject var errorHost: ErrorHost;
+    @State var bundle: Bundle?;
+
+    func loadHex(_ index: Int) {
+        sectionState.showHex(data: document.loadRaw(errorHost: errorHost, section: index));
+    }
+
+    func loadData(_ index: Int) {
+        sectionState.showData(value: document.loadData(errorHost: errorHost, section: index, bundle: bundle!));
+    }
+
+    func loadStrings(_ index: Int) {
+        sectionState.showStrings(value: document.loadStrings(errorHost: errorHost, section: index));
+    }
+
+    func loadStructuredData(_ index: Int) {
+        sectionState.showStructuredData(value: document.loadStructuredData(errorHost: errorHost, section: index));
+    }
+
+    var body: some View {
+        GeometryReader { geo in
+            VStack {
+                HStack {
+                    VStack {
+                        MainView(document: $document, bundle: $bundle)
+                        NavigationView(document: $document, bundle: $bundle, selected: $selected)
+                    }.environmentObject(sectionState)
+                    //Main view
+                    MainSectionView(document: $document)
+                        .environmentObject(sectionState)
+                        .frame(width: geo.size.width * 0.55)
+                        .padding()
+                }
+            }
+        }.onAppear {
+            bundle = document.findBundle();
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(document: .constant(BPXDocument()))
+    }
+}
---
+++ BPXViewer/View/Content/MainHeaderView.swift
@@ -0,0 +1,73 @@
+//
+//  MainHeaderView.swift
+//  BPXViewer
+//
+//  Created by Yuri Edwrad on 5/26/22.
+//
+
+import SwiftUI
+
+struct MainHeaderView: View {
+    @Binding var bundle: Bundle?;
+    var header: bpx_main_header_t?;
+
+    fileprivate func getType() -> String {
+        if header!.ty >= 0x20 && header!.ty <= 0x7E {
+            return String(bytes: [header!.ty], encoding: .ascii)!;
+        } else {
+            return String(format: "%02X", header!.ty);
+        }
+    }
+
+    var body: some View {
+        VStack {
+            if let header = header {
+                if let name = bundle?.main.name {
+                    Text("BPX Main Header").bold()
+                    Text(name).bold().padding(.bottom)
+                } else {
+                    Text("BPX Main Header").bold().padding(.bottom)
+                }
+                HStack {
+                    Text("Version: ").bold()
+                    Spacer()
+                    Text("\(header.version)")
+                        .textSelection(.enabled)
+                }
+                HStack {
+                    Text("Type: ").bold()
+                    Spacer()
+                    Text(getType())
+                        .textSelection(.enabled)
+                }
+                HStack {
+                    Text("Checksum: ").bold()
+                    Spacer()
+                    Text(String(format: "%X", header.chksum))
+                        .textSelection(.enabled)
+                }
+                HStack {
+                    Text("Recorded file size: ").bold()
+                    Spacer()
+                    Text("\(header.file_size)")
+                        .textSelection(.enabled)
+                }
+                HStack {
+                    Text("Number of sections: ").bold()
+                    Spacer()
+                    Text("\(header.section_num)")
+                        .textSelection(.enabled)
+                }
+            } else {
+                Text("No container loaded.")
+            }
+        }
+        .blockView()
+    }
+}
+
+struct MainHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainHeaderView(bundle: .constant(nil), header: bpx_main_header_t())
+    }
+}
